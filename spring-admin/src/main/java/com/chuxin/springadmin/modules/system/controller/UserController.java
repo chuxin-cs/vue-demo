8db08db6d25c94aabed971ff43b4635f8ff0f318
chuxin <15115359055@139.com>
2025-03-25 14:30:52 +0800

    feat: 添加用户信息接口并优化pom.xml配置 在UserController中添加了获取用户信息的接口，并对pom.xml文件进行了清理和优化，移除冗余配置，增加注释以提高可读性

--- spring-admin/src/main/java/com/chuxin/springadmin/modules/system/controller/UserController.java
+++ spring-admin/src/main/java/com/chuxin/springadmin/modules/system/controller/UserController.java
@@ -1,4 +1,16 @@
 package com.chuxin.springadmin.modules.system.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @GetMapping("/info")
+    public String getInfo() {
+        return "info";
+    }
+
 }
