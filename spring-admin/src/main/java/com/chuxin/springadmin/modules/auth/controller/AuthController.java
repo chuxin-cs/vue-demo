20e8b9eb0ce73cac4fca944c6f29dc4d70dd109a
chuxin <15115359055@139.com>
2025-04-03 13:54:22 +0800

    refactor(auth): 移除未使用的Log注解
    移除AuthController中未使用的`@Log`注解，以保持代码简洁和一致性

--- spring-admin/src/main/java/com/chuxin/springadmin/modules/auth/controller/AuthController.java
+++ spring-admin/src/main/java/com/chuxin/springadmin/modules/auth/controller/AuthController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
