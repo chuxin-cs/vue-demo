5bd44d13f4c1fd0550ffa13ff58ea65a4a8181f0
chuxin <15115359055@139.com>
2025-05-19 16:59:32 +0800

    chore: 删除项目中的冗余文件和配置
    删除多个不再使用的文件，包括配置文件、图片资源、前端组件和工具类文件，以清理项目结构并减少不必要的代码冗余。这些文件包括但不限于：
    - 多个图片资源文件（如 logo.png、404.png 等）
    - 前端配置文件（如 .dockerignore、.gitattributes 等）
    - 工具类文件（如 doc.js、bus.js 等）
    - 多个前端组件和页面（如 docx.vue、pdf.vue 等）
    
    清理这些文件有助于提高项目的可维护性和代码整洁度。

--- spring-admin/src/main/java/com/chuxin/springadmin/modules/system/mapper/RoleMapper.java
+++
@@ -1,9 +0,0 @@
-package com.chuxin.springadmin.modules.system.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.chuxin.springadmin.modules.system.model.entity.Role;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface RoleMapper extends BaseMapper<Role> {
-}
--- spring-admin/src/main/java/com/chuxin/springadmin/modules/system/service/RoleService.java
+++
@@ -1,7 +0,0 @@
-package com.chuxin.springadmin.modules.system.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.chuxin.springadmin.modules.system.model.entity.Role;
-
-public interface RoleService extends IService<Role> {
-}
