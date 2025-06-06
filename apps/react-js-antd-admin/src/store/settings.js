448ce545427e29db80a2895920f8fa237abb0bc7
chuxin <15115359055@139.com>
2025-06-06 14:09:14 +0800

    feat(store): 添加设置状态管理模块
    使用zustand创建全局状态管理，用于存储和管理应用的主题、布局等设置
    包含持久化功能，将设置保存在localStorage中

---
+++ apps/react-js-antd-admin/src/store/settings.js
@@ -0,0 +1,40 @@
+import { create } from "zustand"
+import { createJSONStorage, persist } from "zustand/middleware"
+
+const useSettingStore = create()(
+  persist(
+    (set) => ({
+      settings: {
+        themeColorPresets:"default",
+        themeMode: "light",
+				themeLayout: "vertical",
+				themeStretch: false,
+				breadCrumb: true,
+				accordion: false,
+				multiTab: true,
+				darkSidebar: false,
+				fontFamily: "Open Sans Variable",
+				fontSize: 14,
+				direction: "ltr",
+      },
+      actions:{
+        setSettings: (settings) => {
+					set({ settings });
+				},
+				clearSettings() {
+					useSettingStore.persist.clearStorage();
+				},
+      }
+    }),
+    {
+      name: "settings",
+      storage: createJSONStorage(() => localStorage),
+      partialize: (state) => ({
+        "settings": state.settings
+      })
+    }
+  )
+)
+
+export const useSettings = () => useSettingStore((state) => state.settings);
+export const useSettingActions = () => useSettingStore((state) => state.actions);
