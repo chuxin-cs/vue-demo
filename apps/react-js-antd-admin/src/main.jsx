c40ad8071238dcfdd01f98fea3224ab1d667fe87
chuxin <15115359055@139.com>
2025-06-06 16:52:11 +0800

    feat: 国际化

--- apps/react-js-antd-admin/src/main.jsx
+++ apps/react-js-antd-admin/src/main.jsx
@@ -1,14 +1,12 @@
 import { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
-
 // components
 import App from './App.jsx';
 import ProgressBar from './components/ProgressBar';
-
+// i18n
+import './locales/i18n';
 // css
-import "./styles/index.css";
-
-
+import './styles/index.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
