9a225018d08694cd4cca40d70b488bd5e19892c3
chuxin <15115359055@139.com>
2025-06-06 10:31:05 +0800

    refactor: 移除App组件中的冗余代码并添加Suspense
    - 删除App.jsx中无用的div和h1标签
    - 在main.jsx中添加Suspense组件包裹App，以支持懒加载功能

--- apps/react-js-antd-admin/src/main.jsx
+++ apps/react-js-antd-admin/src/main.jsx
@@ -1,4 +1,10 @@
+import { Suspense } from "react"
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(<App />);
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <Suspense>
+    <App />
+  </Suspense>
+);
