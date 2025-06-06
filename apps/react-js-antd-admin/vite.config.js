70d5dc9a822e4e16c13417453dbf608ebff1fbb6
chuxin <15115359055@139.com>
2025-06-06 15:05:19 +0800

    build: 添加vanilla-extract插件及相关依赖
    添加@vanilla-extract/vite-plugin到vite配置中，并安装相关依赖包@vanilla-extract/css和ramda等。这些变更将为项目提供CSS-in-JS解决方案和函数式编程工具库支持。

--- apps/react-js-antd-admin/vite.config.js
+++ apps/react-js-antd-admin/vite.config.js
@@ -2,12 +2,16 @@ import path from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    vanillaExtractPlugin({
+      identifiers: ({ debugId }) => `${debugId}`,
+    }),
   ],
   server: {
     port: 3001
