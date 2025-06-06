368a52af51b1cae1392456b2d36d9a0a40025a29
chuxin <15115359055@139.com>
2025-06-06 11:11:42 +0800

    feat: 添加路由和仪表盘布局组件
    - 在App.jsx中引入并渲染路由组件
    - 重构仪表盘布局，使用Ant Design Layout组件并添加加载状态
    - 将Main组件作为仪表盘的主要内容区域

--- apps/react-js-antd-admin/src/App.jsx
+++ apps/react-js-antd-admin/src/App.jsx
@@ -1,6 +1,10 @@
+import Router from '@/router';
+
 function App() {
   return (
-   
+    <>
+      <Router />
+    </>
   );
 }
 
--- apps/react-js-antd-admin/src/layouts/dashboard/index.jsx
+++ apps/react-js-antd-admin/src/layouts/dashboard/index.jsx
@@ -1,9 +1,19 @@
+import { Layout } from 'antd';
+import { Suspense } from 'react';
+
+// components
+import Main from './Main';
+
 const DashboardLayout = () => {
   return (
-    <div>
-      DashboardLayout
-    </div>
+    <Layout>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Layout>
+          <Main />
+        </Layout>
+      </Suspense>
+    </Layout>
   );
-}
+};
 
 export default DashboardLayout;
