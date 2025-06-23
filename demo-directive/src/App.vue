d88b83538b0063463a6c781aa3c4da565a539ff1
chuxin <15115359055@139.com>
2025-06-23 09:41:09 +0800

    feat(指令): 添加防抖指令并应用到提交按钮
    在main.ts中引入防抖指令并注册，在App.vue中添加提交按钮应用该指令

--- demo-directive/src/App.vue
+++ demo-directive/src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+const submit = () => {
+  console.log('提交');
+}
 </script>
 
 <template>
   <div>
-    1
+    <h1>Vue Directive</h1>
+    <button v-debounce:2000.leading="submit">提交</button>
   </div>
 </template>
--- demo-directive/src/main.ts
+++ demo-directive/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-
 const app = createApp(App);
+
+// 引入防抖指令
+import debounceDirective from './directives/debounce';
+
+app.directive('debounce', debounceDirective);
 app.mount('#app');
