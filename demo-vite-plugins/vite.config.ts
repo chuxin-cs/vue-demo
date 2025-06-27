95b69a4a939449be6c24f5878a25dcae76054417
chuxin <15115359055@139.com>
2025-06-27 10:30:17 +0800

    feat: 初始化Vite + Vue + TS项目模板
    添加基础项目结构，包括Vite配置、Vue组件和样式
    引入code-inspector-plugin插件用于开发调试
    更新README说明项目用途和配置

--- demo-vite-plugins/vite.config.ts
+++ demo-vite-plugins/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { codeInspectorPlugin } from 'code-inspector-plugin';
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    codeInspectorPlugin({
+      bundler: 'vite',
+      editor: 'cursor',
+      showSwitch: true
+    })
   ],
 })
