9df0e916a51d278904e11ac0c4402130a39ea1ed
chuxin <15115359055@139.com>
2025-06-30 17:40:12 +0800

    feat: 添加用户年龄计算属性并更新显示方式
    添加计算属性 age 来格式化用户年龄显示，并在模板中使用该计算属性
    添加更新用户信息的按钮，支持通过 updateUser 方法更新年龄

--- demo-pinia/src/App.vue
+++ demo-pinia/src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import {computed} from "vue"
 import {useUserStore} from "@/store"
-
 const userStore = useUserStore()
+const age = computed(()=>{
+  return `age: ${userStore.user.age} 岁`
+})
 </script>
 
 <template>
@@ -11,9 +14,8 @@ const userStore = useUserStore()
     <!-- 用户user.ts -->
     <h2>用户：user</h2>
     <p>name: {{ userStore.user.name }}</p>
-    <input type="text" v-model="userStore.user.name">
-    <p>age: {{ userStore.user.age }}</p>
-
-
+    <input type="text" v-model="userStore.user.name" />
+    <p>{{age}}</p>
+    <button @click="userStore.updateUser({age: 28})">更新用户信息</button>
   </div>
 </template>
