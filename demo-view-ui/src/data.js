f5f0ac1a2ec08be4f2e7300d0a89cce203ba8c32
chuxin <15115359055@139.com>
2025-07-22 16:37:42 +0800

    refactor(data.js): 简化模拟数据生成逻辑
    移除随机生成数据的逻辑，改为使用固定值，提高测试数据的确定性

--- demo-view-ui/src/data.js
+++ demo-view-ui/src/data.js
@@ -61,28 +61,15 @@ export const data12 = [
 
 // 定义一个函数用于生成模拟数据
 function generateMockData(count) {
-  const names = ['John Brown', 'Joe Black', 'Jim Green', 'Jon Snow'];
-  const addresses = [
-    'New York No. 1 Lake Park',
-    'London No. 1 Lake Park',
-    'Sydney No. 1 Lake Park',
-    'Ottawa No. 2 Lake Park'
-  ];
   const result = [];
-
   for (let i = 0; i < count; i++) {
-    const id = `1010${i}`;
-    const randomNameIndex = Math.floor(Math.random() * names.length);
-    const randomAge = Math.floor(Math.random() * 20) + 18; // 随机生成18 - 37岁的年龄
-    const randomAddressIndex = Math.floor(Math.random() * addresses.length);
-
+    const id = `1012${i}`;
     result.push({
       id,
-      name: names[randomNameIndex],
-      age: randomAge,
-      address: addresses[randomAddressIndex]
+      name: "我是name",
+      age: 28,
+      address: "我是地址"
     });
   }
-
   return result;
 }
